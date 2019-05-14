6c19e4bd018cca3d8f980bcfe6ef167cafb591df
lnalborczyk <ladislas.nalborczyk@gmail.com>
2019-05-14 15:35:43 +0200

    drafting cognitive theories of rumination

--- chapter_word_output.R
+++ chapter_word_output.R
@@ -5,6 +5,5 @@ preview_chapter(
   output_format = "bookdown::word_document2",
   output_file = paste0("thesis-intro-", format(Sys.Date(), ("%d-%m-%y") ), ".docx")
   #output_dir = "chapter_preview"
-  #new_session = TRUE
   #output_options = list(reference_docx = "word-style-ref.docx")
   )
